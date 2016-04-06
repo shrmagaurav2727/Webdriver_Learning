62059043cc6fe8b1286ea9f69fb204da96f9213e
shrmagaurav2727 <sharma.gaurav2727@gmail.com>
2016-04-06 18:44:41 +0530

    test datdded

---
+++ Test/src/polyMorphsm/compileTime.java
@@ -0,0 +1,27 @@
+package polyMorphsm;
+
+public class compileTime {
+
+	public int call1(int x, int y) {
+		// TODO Auto-generated method stub
+return x+y;
+		
+}
+
+	
+	
+	
+public int call2(int x, int y, int c){
+	
+	return x+y+c;
+}
+	
+	
+public long call3(long x, int y, int c )
+
+{
+	return x+y+c;
+}
+	
+	
+}
